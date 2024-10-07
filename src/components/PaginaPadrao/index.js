52a36d4db9fb91c71a1fa114c3b2f38db3be858b
lucas <andrebastoslucas@gmail.com>
2024-10-07 16:36:43 -0300

    aplicando redux

---
+++ src/components/PaginaPadrao/index.js
@@ -0,0 +1,15 @@
+import NavBar from "components/Navbar";
+import styles from "./PaginaPadrao.module.scss";
+import { Outlet } from "react-router-dom";
+import Footer from "components/Footer";
+export default function PaginaPadrao() {
+  return (
+    <div className={styles.container}>
+      <NavBar />
+      <div className={styles["container-outlet"]}>
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+}
