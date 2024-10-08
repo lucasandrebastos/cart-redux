3193260108d146856c6131d5c8129bf89e9dd459
lucas <andrebastoslucas@gmail.com>
2024-10-08 16:47:41 -0300

    feat: readme

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -1,33 +1,50 @@
-import { useSelector } from "react-redux";
+import Header from "components/Header";
 import styles from "./Carrinho.module.scss";
+import { useSelector, useDispatch } from "react-redux";
 import Item from "components/Item";
+import { resetarCarrinho } from "store/reducers/carrinho";
+
 export default function Carrinho() {
-  const carrinho = useSelector((state) => {
-    const carrinhoReduce = state.carrinho.reduce((item, itemNoCarrinho) => {
+  const dispatch = useDispatch();
+  const { carrinho, total } = useSelector((state) => {
+    let total = 0;
+    const regExp = new RegExp(state.busca, "i");
+    const carrinhoReduce = state.carrinho.reduce((itens, itemNoCarrinho) => {
       const item = state.itens.find((item) => item.id === itemNoCarrinho.id);
-      itens.push({
-        ...item,
-        quantidade: itemNoCarrinho.quantidade,
-      });
+      total += item.preco * itemNoCarrinho.quantidade;
+      if (item.titulo.match(regExp)) {
+        itens.push({
+          ...item,
+          quantidade: itemNoCarrinho.quantidade,
+        });
+      }
+
+      return itens;
     }, []);
-    return carrinhoReduce;
+    return { carrinho: carrinhoReduce, total: total };
   });
   return (
     <div>
       <Header
         titulo="Carrinho de compras"
-        descricao="Confira os produtos que voce adicionou no carrinho"
+        descricao="Confira produtos que você adicionou ao carrinho."
       />
       <div className={styles.carrinho}>
         {carrinho.map((item) => (
-          <Item key={item.id} {...item} />
+          <Item key={item.id} {...item} carrinho />
         ))}
         <div className={styles.total}>
           <strong>Resumo da compra</strong>
           <span>
-            Subtotal: <strong>R$ {(0.0).toFixed(2)}</strong>
+            Subtotal: <strong> R$ {total.toFixed(2)} </strong>
           </span>
         </div>
+        <button
+          className={styles.finalizar}
+          onClick={() => dispatch(resetarCarrinho())}
+        >
+          Finalizar compra
+        </button>
       </div>
     </div>
   );
