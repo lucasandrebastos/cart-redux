706a5af53968c53104681dae02939bfca33233a3
lucas <andrebastoslucas@gmail.com>
2024-10-08 14:11:12 -0300

    feat: redux nos estados do carrinho

---
+++ src/components/Item/index.js
@@ -0,0 +1,65 @@
+import styles from "./Item.module.scss";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { FaCartPlus } from "react-icons/fa";
+import { mudarFavorito } from "store/reducers/itens";
+import { useDispatch, useSelector } from "react-redux";
+import { mudarCarrinho } from "store/reducers/carrinho";
+
+const iconeProps = {
+  size: 24,
+  color: "#041833",
+};
+
+export default function Item(props) {
+  const { titulo, foto, preco, descricao, favorito, id } = props;
+  const dispatch = useDispatch();
+  const temNoCarrinho = useSelector((state) =>
+    state.carrinho.some((itemNoCarrinho) => itemNoCarrinho.id === id)
+  );
+
+  function toggleFavorite() {
+    dispatch(mudarFavorito(id));
+  }
+  function resolverCarrinho() {
+    dispatch(mudarCarrinho(id));
+  }
+
+  return (
+    <div className={styles.item}>
+      <div className={styles["item-imagem"]}>
+        <img src={foto} alt={titulo} />
+      </div>
+      <div className={styles["item-descricao"]}>
+        <div className={styles["item-titulo"]}>
+          <h2>{titulo}</h2>
+          <p>{descricao}</p>
+        </div>
+        <div className={styles["item-info"]}>
+          <div className={styles["item-preco"]}>R$ {preco.toFixed(2)}</div>
+          <div className={styles["item-acoes"]}>
+            {favorito ? (
+              <AiFillHeart
+                {...iconeProps}
+                color="#ff0000"
+                className={styles["item-acao"]}
+                onClick={toggleFavorite}
+              />
+            ) : (
+              <AiOutlineHeart
+                {...iconeProps}
+                className={styles["item-acao"]}
+                onClick={toggleFavorite}
+              />
+            )}
+            <FaCartPlus
+              {...iconeProps}
+              color={temNoCarrinho ? "#1875E8" : iconeProps.color}
+              className={styles["item-acao"]}
+              onClick={resolverCarrinho}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoriasSlice from "./reducers/categorias";
 import itensSlice from "./reducers/itens";
+import carrinhoSlice from "./reducers/carrinho";
 const store = configureStore({
   reducer: {
     categorias: categoriasSlice,
     itens: itensSlice,
+    carrinho: carrinhoSlice,
   },
 });
 
